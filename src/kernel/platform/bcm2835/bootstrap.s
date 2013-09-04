c86dc4666cfabc4e9c564414fa8d6686bf6e979e
Sven-Kristofer Pilz <sven.pilz@gmail.com>
2013-09-04 21:52:02 +0200

    Set the stack for FIQ mode.

--- src/kernel/platform/bcm2835/bootstrap.s
+++ src/kernel/platform/bcm2835/bootstrap.s
@@ -1,8 +1,26 @@
 .section .text.bootstrap
 .global _start
 _start:
+	@
+	@ set the stack for FIQ mode
+	@
+	mov r4, #0xD1   @ fiq mode with no interrupts
+	msr cpsr_c,r4
+	mov sp, #0x4000
+
+	@
+	@ set the stack for supervisor mode
+	@
+	mov r4, #0xD3   @ supervisor mode with no interrupts
+	msr cpsr_c, r4
 	mov sp, #0x8000
+
+	@
+	@ jump to kernel main
+	@
 	bl main
+
 halt:
 	wfe
 	b halt
+
