9e380aa76bda41f80637d6585d56b85f0e033d25
Sven-Kristofer Pilz <sven.pilz@gmail.com>
2013-10-14 17:59:41 +0200

    Let delay be dummy on local unit test.
    It depends on ARM assembler code.

--- src/lib/c/delay.c
+++ src/lib/c/delay.c
@@ -1,8 +1,10 @@
 #include <delay.h>
 
 void delay(int32_t count) {
+	#ifdef __arm__
 	asm volatile(
 		"__delay_%=: subs %[count], %[count], #1\n"
 		"bne __delay_%=\n"
 	: : [count]"r"(count) : "cc");
+	#endif
 }
